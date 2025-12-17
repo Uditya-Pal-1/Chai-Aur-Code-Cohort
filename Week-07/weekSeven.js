2427ee9e74956c10dfcc5a0440a2b7a1bc02da77
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2025-12-17 22:22:59 +0530

    week 7

---
+++ Week-07/weekSeven.js
@@ -0,0 +1,12 @@
+//weekSeven.js
+
+//alert message to indicate the script is loaded.
+alert("This is week 7 JavaScript file!");
+
+// for quokka extension use in vs code for javascript command is ctrl + k + j.
+
+//log a message to the console.
+console.log("Week 7 script loaded successfully.!");
+
+const x = 10;
+console.log(x * x);
