1159f1decaf8720a8b16b954c76aa58b5991507c
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2026-05-04 07:26:59 +0530

    done👍

--- Week-11/Lexical_ScopingAndClosures/createCounter.js
+++ Week-11/Lexical_ScopingAndClosures/createCounter.js
@@ -1,14 +1,42 @@
-function createCounter(stepSize=1,initialValue = 0){
-    return function(){
-        initialValue+=stepSize;
+function createCounter(stepSize = 1, initialValue = 0) {
+    return function () {
+        initialValue += stepSize;
         return initialValue;
     }
 }
 
-const i1 = createCounter(1,1)
+const i1 = createCounter(1, 1)
 
 console.log(i1())
 console.log(i1())
 console.log(i1())
 console.log(i1())
 console.log(i1())
+
+function debouncedVersion(fn, delay) {
+    let timerId = null
+    return function () {
+        createTimeout(timerId)
+        setTimeout(fn, delay)
+    }
+}
+
+function apiCall(){}
+const apiCallDebounce = debouncedVersion(apiCall, 5*1000)
+apiCallDebounce()
+
+function createInstance(){
+    let store = {
+        value:100,
+    }
+    return function(){
+        console.log(store);
+    }
+}
+
+const logger = createInstance()
+logger();
+logger();
+logger();
+logger();
+logger();
