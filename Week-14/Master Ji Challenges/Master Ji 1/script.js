2622f54b66b736f27d2a8604c246c1a8dd54769c
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2026-07-28 17:22:03 +0530

    done 👍

--- Week-14/Master Ji Challenges/Master Ji 1/script.js
+++ Week-14/Master Ji Challenges/Master Ji 1/script.js
@@ -1,7 +1,7 @@
-const moodBtn = document.querySelector(".moodBtn");
-const saveBtn = document.getElementById("saveMessage");
-// const timelineBtn = document.querySelector(".timelinebtn");
-// const calendarBtn = document.querySelector('.calendarBtn');
+const moodBtns = document.querySelectorAll(".moodBtn");
+const saveMessage = document.getElementById("saveMessage");
+const timelineBtn = document.querySelector(".timelinebtn");
+const calendarBtn = document.querySelector('.calendarBtn');
 const timelineView = document.getElementById('timelineView');
 const calendarView = document.getElementById('calendarView');
 
@@ -32,10 +32,93 @@ function saveLog(mood, emoji){
     renderCalendar();
 }
 
-moodBtn.forEach(btn =>{
+moodBtns.forEach(btn =>{
     btn.addEventListener('click', (e) =>{
         const moodText = e.target.getAttribute('data-mood')
         const emojiFace = e.target.innerText;
         saveLog(moodText,  emojiFace);
     })
 })
+
+function renderTimeline(){
+    const logs = getLogs();
+    timelineView.innerHTML = '';
+
+    if(logs.length === 0){
+        timelineView.innerHTML = '<p>No Mood Yet. Click On Add Mood </p>'
+        return;
+    }
+    logs.sort((a,b) => new Date(b.date) - new Date(a.date))
+    logs.forEach(log =>{
+        const div = document.createElement('div')
+        div.classList.add('timeline-item')
+        div.innerHTML = `
+        <span><strong>${log.date}</strong></span>
+        <span>${log.emoji} ${log.mood}</span>
+        `;
+        timelineView.appendChild(div);
+    })
+}
+
+function renderCalendar(){
+    const logs = getLogs();
+    calendarView.innerHTML = '';
+
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfWeek = new Date(year, month, 1).getDay();
+
+    let calendarHTML = `<h3> Month: ${month + 1}/ ${year}</h3>`
+    calendarHTML +=`<div class='calendar-grid'>`;
+
+    const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+    days.forEach(day =>calendarHTML += `<div class = 'day-header'>${day}</div>`)
+
+    for (let i = 0; i < firstDayOfWeek; i++){
+        calendarHTML += `<div class = 'calendar-cell'></div>`
+    }
+    
+    for(let day = 1; day <= daysInMonth; day++){
+        const paddedMonth = String(month + 1).padStart(2, '0')
+        const paddedDay = String(day).padStart(2, '0')
+        const currentDateStr = `${year}-${paddedMonth}-${paddedDay}`
+        
+        const matchedLog = logs.find(log => log.date===currentDateStr);
+        if (matchedLog){
+            calendarHTML += `
+            <div class = 'calendar-cell has-mood'>
+            <span>${day}</span>
+            <span>${matchedLog.emoji}</span>
+            </div>`
+        }else{
+            calendarHTML += `
+            <div class='calendar-cell'>
+            <span>${day}</span>
+            </div>`
+        }
+    }
+    calendarHTML += `</div>`
+    calendarView.innerHTML = calendarHTML;
+};
+
+timelineBtn.addEventListener('click',()=>{
+    timelineView.classList.remove('hidden');
+    calendarView.classList.add('hidden')
+
+    timelineView.classList.add('active')
+    calendarView.classList.remove('active')
+});
+
+calendarBtn.addEventListener('click', () =>{
+    calendarView.classList.remove('hidden')
+    timelineView.classList.add('hidden')
+
+    timelineView.classList.add('active')
+    calendarView.classList.remove('active')
+});
+
+renderTimeline();
+renderCalendar();
