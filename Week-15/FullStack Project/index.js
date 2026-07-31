92343b041f14e6b98bd743f7b69a4415d10188e2
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2026-07-31 20:14:57 +0530

    boiler plate express, mongodb etc

---
+++ Week-15/FullStack Project/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import db from './utils/db.js';
+ 
+dotenv.config();
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        credentials: true,
+        methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+  
+
+// const port = 3000; // also we use ports are - 4000, 5000, 5173, 8080, 8000 etc
+// not to use are - 443, 80, 23 etc.
+
+app.get('/', (req, res) => {
+    res.send('Cohort !')
+})
+
+app.get('/Uditya', (req, res) => {
+    res.send('Uditya Pal')
+})
+
+app.get('/Aman', (req, res) => {
+    res.send('Aman Pal')
+})
+console.log(process.env.PORT)
+
+// connect to db
+db();
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
