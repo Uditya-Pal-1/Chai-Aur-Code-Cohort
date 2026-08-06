f5e78be9467d87b50fcad14a93cd578738748606
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2026-08-06 22:40:29 +0530

    done 👍

--- Week-16/FullStackSQL/controllers/auth.controller.js
+++ Week-16/FullStackSQL/controllers/auth.controller.js
@@ -1,7 +1,10 @@
-import { prismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 import bcrypt from 'bcryptjs';
-import crypto from 'crypto'
-const prisma = new prismaClient();
+import crypto from 'crypto';
+import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
+
+const prisma = new PrismaClient();
 
 export const registerUser = async (req, res) => {
     const { name, email, password, phone } = req.body
@@ -20,7 +23,7 @@ export const registerUser = async (req, res) => {
         if (existingUser) {
             return res.status(400).json({
                 success: false,
-                message: 'All fields are required'
+                message: 'User already exist with this email '
             })
         }
         //password hashing
@@ -37,11 +40,46 @@ export const registerUser = async (req, res) => {
 
             }
         })
-        //mail by nodemailer
+        //send mail by nodemailer
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAILTRAP_HOST,
+            port: process.env.MAILTRAP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.MAILTRAP_USERNAME,
+                pass: process.env.MAILTRAP_PASSWORD,
+            }
+        })
 
+        const verificationURL = `${process.env.BASE_URL}/api/v1/users/verify/${verificationToken}`
+
+       const mailOptions = {
+            from: process.env.MAILTRAP_SENDEMAIL,
+            to: user.email,
+            subject: 'Verify your email address',
+            text: `Welcome ${user.name}! Please click on the following link to verify your email account: \n\n${verificationUrl}`,
+        };
+
+        // Send mail
+        await transporter.sendMail(mailOptions);
+
+        // Final success response
+        return res.status(201).json({
+            success: true,
+            message: 'User registered successfully. Please check your email to verify your account.',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        });
 
     } catch (error) {
-
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            message: 'user unable to register.'
+        })
     }
 };
 
--- Week-16/FullStackSQL/routes/auth.router.js
+++ Week-16/FullStackSQL/routes/auth.router.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { registerUser } from '../controllers/auth.controller'
 
+
 const userRouter = express.Router()
+
 
 userRouter.post('/register', registerUser)
 
