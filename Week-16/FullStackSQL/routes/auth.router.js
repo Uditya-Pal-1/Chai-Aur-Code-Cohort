f2b4009fb8d93cf9f1ed64192aaf78ce3658972f
Uditya Pal <149376004+Uditya-Pal-1@users.noreply.github.com>
2026-08-06 22:42:11 +0530

    done 👍

--- Week-16/FullStackSQL/routes/auth.router.js
+++ Week-16/FullStackSQL/routes/auth.router.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import { registerUser } from '../controllers/auth.controller'
 
-
 const userRouter = express.Router()
-
 
 userRouter.post('/register', registerUser)
 
