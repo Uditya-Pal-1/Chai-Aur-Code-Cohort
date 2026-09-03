937c60e2578d91fc0e3640daeec6237f184236e7
Uditya-Pal-1 <amanpal5035286@gmail.com>
2026-09-03 21:45:10 +0530

    note controller

--- Week-17/Backend Project/src/controllers/note.controllers.js
+++ Week-17/Backend Project/src/controllers/note.controllers.js
@@ -1,21 +1,25 @@
+import {Project} from '../models/project.models.js';
+
+//get all notes
 const getNotes = async(req, res)=>{
-    //get all notes
+const {projectId} = req.params;
+const project = await Project.findById(projectId)
 }
 
+//get Notes by Id.
 const getNotesById = async(req, res)=>{
-    //get Notes by Id.
 }
 
+//create Notes
 const createNotes = async(req, res)=>{
-    //create Notes
 }
 
+//update Notes
 const updateNotes = async(req, res)=>{
-    //update Notes
 }
 
+//delete Notes
 const deleteNotes = async(req, res)=>{
-    //delete Notes
 }
 
 export {getNotes, getNotesById, createNotes, updateNotes, deleteNotes,};
