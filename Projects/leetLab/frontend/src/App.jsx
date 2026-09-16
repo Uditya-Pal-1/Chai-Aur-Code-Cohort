069f0b271823559ebda1fdfd632115c7ed8affc6
Uditya-Pal-1 <amanpal5035286@gmail.com>
2026-09-16 21:48:08 +0530

    imp files

---
+++ Projects/leetLab/frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import SignupPage from './Pages/SignupPage'
+import LoginPage from './Pages/LoginPage'
+import HomePage from './Pages/HomePage'
+
+const App = () => {
+  let authUser = null;
+
+  return (
+    <div className='flex flex-col items-center justify-start'>
+      <Routes>
+
+        <Route
+        path='/'
+        element={authUser ? <HomePage /> : <Navigate to='/login' />}
+        />
+
+        <Route
+        path='/login'
+        element={ !authUser ? <LoginPage/> : <Navigate to='/'/>}
+        />
+
+        <Route
+        path='/signup'
+        element={!authUser ? <SignupPage/> : <Navigate to='/'/>}
+        />
+
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
