f2058b205727b571a68f75744d933911f37d620f
Uditya-Pal-1 <amanpal5035286@gmail.com>
2026-09-22 21:06:32 +0530

    learning React

--- Week-19/FullStack/Frontend/src/App.jsx
+++ Week-19/FullStack/Frontend/src/App.jsx
@@ -7,9 +7,11 @@ function App() {
 
   return (
     <>
-    <h1>Welcome to our fontend application</h1>
+    <div>
+    <h1 className=' flex justify-center font-bold font-mono text-4xl text-red-600 p-1 m-2 flex-wrap border-2 border-blue-500 border- '>Welcome to our fontend application</h1>
     {name}
-    <button onClick={()=>{setName("Okay")}}>Change Name</button>
+    <button className='bg-yellow-400 py-2 px-3 m-4' onClick={()=>{setName("Okay")}}>Change Name</button>
+    </div>
     </>
   )
 }
